3c0052e3469e760f88aa37db4d9b92a2eabe53fa
mizchi <miz404@gmail.com>
2023-05-19 14:50:47 +0900

    Support host

--- zero-runtime/src/fetch.test.ts
+++ zero-runtime/src/fetch.test.ts
@@ -1,11 +1,11 @@
 import { test } from "vitest";
-import { TypedFetch } from "./fetch";
+import { RequestInitT, TypedFetch } from "./fetch";
 import { JSON$stringifyT } from "./json";
 
 test.skip("check types only", async () => {
   const stringifyT = JSON.stringify as JSON$stringifyT;
-  const fetch = window.fetch as TypedFetch<{
-    "/api/:xxx": {
+  type Api = {
+    "/api/:id": {
       method: "GET";
       bodyType: { text: string; number: number; boolean: boolean };
       headersType: { "Content-Type": "application/json" };
@@ -22,18 +22,57 @@ test.skip("check types only", async () => {
       headersType: { "Content-Type": "application/json" };
       responseType: { text: string; number: number; boolean: boolean };
     };
+  };
+  const fetch = window.fetch as TypedFetch<{
+    "": Api;
+    "http://localhost:8080": Api;
+    "https://example.com": Api;
+    // "/api/:xxx": {
+    //   method: "GET";
+    //   bodyType: { text: string; number: number; boolean: boolean };
+    //   headersType: { "Content-Type": "application/json" };
+    //   responseType: { text: string; number: number; boolean: boolean };
+    // } | {
+    //   method: "POST";
+    //   bodyType: { postData: number };
+    //   headersType: { "Content-Type": "application/json" };
+    //   responseType: { ok: boolean };
+    // };
+    // "/api/nested/:pid": {
+    //   method: "GET";
+    //   bodyType: { nested: 1 };
+    //   headersType: { "Content-Type": "application/json" };
+    //   responseType: { text: string; number: number; boolean: boolean };
+    // };
   }>;
-  const res = await fetch("/api/xxxeuoau", {
+
+  type ResponseBody = { text: string; number: number; boolean: boolean };
+  const requestInit = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: stringifyT({ text: "text", number: 1, boolean: true }),
+  } as const;
+
+  const response1 = await fetch("/api/xxxeuoau", requestInit);
+  const _data1: ResponseBody = await response1.json();
+  const response2 = await fetch(
+    "http://localhost:8080/api/11111111",
+    requestInit,
+  );
+  const _data2: ResponseBody = await response2.json();
+
+  // with host
+  await fetch("http://localhost:8080/api/xxxeuoau", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
     body: stringifyT({ text: "text", number: 1, boolean: true }),
   });
-  const _data: { text: string; number: number; boolean: boolean } = await res
-    .json();
 
-  fetch("/api/nested/aaa", {
+  await fetch("/api/nested/aaa", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
--- zero-runtime/src/fetch.ts
+++ zero-runtime/src/fetch.ts
@@ -16,7 +16,7 @@ export interface ResponseT<T> extends Response {
 }
 
 // Extract URL pattern
-export type PathPattern<T extends string | number | symbol> = T extends
+export type PathPattern<T extends string> = T extends
   `/${infer Head}/${infer Body}/${infer Tail}`
   ? Tail extends "" ? `${PathPattern<`/${Head}`>}${PathPattern<`${Body}`>}`
     // folding to 2 term
@@ -30,51 +30,67 @@ export type PathPattern<T extends string | number | symbol> = T extends
   : T;
 
 // Check if the pattern is exact
-export type ExactPathPattern<Static extends string, Statement extends string> =
-  Static extends `${Statement}/${string}` ? false : true;
+export type ExactPattern<Input extends string, Pattern extends string> =
+  Input extends `${Pattern}/${string}` ? false : true;
 
 export type FetchEffectType<
   Method extends string = string,
-  bodyType extends {} = any,
-  headersType extends {} = any,
-  responseType extends {} = any,
+  BodyType extends {} = any,
+  HeadersType extends {} = any,
+  ResponseType extends {} = any,
 > = {
-  method: string;
-  bodyType: any;
-  headersType: any;
-  responseType: any;
+  method: Method;
+  bodyType: BodyType;
+  headersType: HeadersType;
+  responseType: ResponseType;
 };
 
 export type FetchEffectMap = {
-  [pattern: string]: FetchEffectType;
+  [host: string]: {
+    [pattern: string]: FetchEffectType;
+  };
 };
 
-export type TypedFetch<FEMap extends FetchEffectMap> = <
+export type TypedFetch<EffectMap extends FetchEffectMap> = <
   Input extends string,
   Method extends string,
   // filter route
-  ActiveRouteMap extends {
-    [P in keyof FEMap]: Input extends PathPattern<P>
-      ? FEMap[P] & { _pattern: P }
-      : never;
-  }[keyof FEMap],
+  A1 extends {
+    [Host in keyof EffectMap]: {
+      [Pattern in keyof EffectMap[Host]]:
+        ExtractPattern<Input, _StrKey<Host>> extends
+          PathPattern<_StrKey<Pattern>> ? EffectMap[Host][Pattern] & {
+            __pattern: _StrKey<Pattern>;
+            __host: ExtractHost<Input, _StrKey<Pattern>>;
+          }
+          : never;
+    };
+  }[keyof EffectMap][keyof EffectMap[keyof EffectMap]],
   // filter method
-  ActiveMethodMap extends {
-    [P in keyof ActiveRouteMap]: Method extends ActiveRouteMap["method"]
-      ? ActiveRouteMap
-      : never;
-  }[keyof ActiveRouteMap],
+  A2 extends {
+    [P in keyof A1]: Method extends A1["method"] ? A1 : never;
+  }[keyof A1],
   // filter exact
-  ActiveExactMap extends {
-    [P in keyof ActiveMethodMap]:
-      ExactPathPattern<Input, PathPattern<ActiveMethodMap["_pattern"]>> extends
-        true ? ActiveMethodMap : never;
-  }[keyof ActiveMethodMap],
+  A3 extends {
+    [P in keyof A2]: ExactPattern<
+      // Filter pattern fills all input
+      ExtractPattern<Input, A2["__host"]>,
+      PathPattern<A2["__pattern"]>
+    > extends true ? A2 : never;
+  }[keyof A2],
 >(
   input: Input,
   init: RequestInitT<
     Method,
-    ActiveExactMap["bodyType"],
-    ActiveExactMap["headersType"]
+    A3["bodyType"],
+    A3["headersType"]
   >,
-) => Promise<ResponseT<ActiveExactMap["responseType"]>>;
+) => Promise<ResponseT<A3["responseType"]>>;
+
+type ExtractPattern<Input extends string, Host extends string> = Input extends
+  `${Host}${infer Pattern}` ? Pattern : never;
+type ExtractHost<Input extends string, Pattern extends string> = Input extends
+  `${infer Host}${Pattern}` ? Host : never;
+
+type _StrKey<Key extends string | number | symbol> = Key extends string ? Key
+  : never;
